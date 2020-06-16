30cc2929384d7930ae1ca0c8d0b51bf3334abf42
Elliott Lovell <elliott@repositpower.com>
2020-06-16 21:49:20 +1000

    Changing cors

--- medapi/wsgi.py
+++ medapi/wsgi.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}}, expose_headers="Content-Type")
 
 
 @app.route('/', methods=["GET"])
