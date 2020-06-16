36680ad1d825868593b50e3114c8ff4ebd0ca60c
Elliott Lovell <elliott@repositpower.com>
2020-06-16 20:45:29 +1000

    CORS stuff

--- medapi/wsgi.py
+++ medapi/wsgi.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route('/', methods=["GET"])
