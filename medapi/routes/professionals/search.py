fbe671ccce79ac1b8e98ef50acdb437beabfd708
Elliott Lovell <elliott@repositpower.com>
2020-06-16 20:23:39 +1000

    Added cors to professionals routes

--- medapi/routes/professionals/search.py
+++ medapi/routes/professionals/search.py
@@ -2,6 +2,7 @@
 Api routes for searching professionals
 """
 from flask import request
+from flask_cors import cross_origin
 
 from medapi.utils import proxy_request
 from medapi.wsgi import app
@@ -10,10 +11,12 @@ prefix = "/api/professionals"
 
 
 @app.route(prefix + '/list_professionals/', methods=["GET"])
+@cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
 def list_professionals():
     return proxy_request(request)
 
 
 @app.route(prefix + '/list_profession_types/', methods=["GET"])
+@cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
 def list_profession_types():
     return proxy_request(request)
