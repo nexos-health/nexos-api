5c8da95e95699b4dfaddc53e8ad03a7191d7df07
Elliott Lovell <elliott@repositpower.com>
2020-07-29 19:15:42 +1000

    Updated some group routes

--- medapi/routes/professionals/groups.py
+++ medapi/routes/professionals/groups.py
@@ -27,25 +27,34 @@ def create_group(**kwargs):
     return proxy(data=data)
 
 
+@app.route(prefix + '/', methods=["DELETE"])
+@requires_auth
+def delete_group(**kwargs):
+    # user_key_dict = {"user_key": kwargs.get("user_key")}
+    user_key_dict = {"user_key": "UK4BSGB9"}
+    data = {**request.json, **user_key_dict}
+    return proxy(data=data)
+
+
 @app.route(prefix + '/<id>/', methods=["GET"])
 @requires_auth
 def get_group(id, **kwargs):
     return proxy()
 
 
-@app.route(prefix + '/add_professional/', methods=["POST"])
+@app.route(prefix + '/add_professionals/', methods=["POST"])
 @requires_auth
-def add_professional(**kwargs):
+def add_professionals(**kwargs):
     # user_key_dict = {"user_key": kwargs.get("user_key")}
     user_key_dict = {"user_key": "UK4BSGB9"}
-    data = {**request.data, **user_key_dict}
+    data = {**request.json, **user_key_dict}
     return proxy(data=data)
 
 
-@app.route(prefix + '/remove_professional/', methods=["DELETE"])
+@app.route(prefix + '/remove_professionals/', methods=["DELETE"])
 @requires_auth
-def remove_professional(**kwargs):
+def remove_professionals(**kwargs):
     # user_key_dict = {"user_key": kwargs.get("user_key")}
     user_key_dict = {"user_key": "UK4BSGB9"}
-    data = {**request.data, **user_key_dict}
+    data = {**request.json, **user_key_dict}
     return proxy(data=data)
