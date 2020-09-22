f65eeb61fbe37ab28fe83ed1d81a647db3cb03b1
Elliott Lovell <elliott@repositpower.com>
2020-09-22 13:08:28 +1000

    Added edit_group route

--- medapi/routes/professionals/groups.py
+++ medapi/routes/professionals/groups.py
@@ -13,25 +13,30 @@ prefix = "/api/groups"
 @app.route(prefix + '/', methods=["GET"])
 @requires_auth
 def get_groups(**kwargs):
-    # params = {"user_key": kwargs.get("user_key")}
-    params = {"user_key": "UK4BSGB9"}
+    params = {"user_key": kwargs.get("user_key")}
     return proxy(params=params)
 
 
 @app.route(prefix + '/', methods=["POST"])
 @requires_auth
 def create_group(**kwargs):
-    # user_key_dict = {"user_key": kwargs.get("user_key")}
-    user_key_dict = {"user_key": "UK4BSGB9"}
+    user_key_dict = {"user_key": kwargs.get("user_key")}
     data = {**request.json, **user_key_dict}
     return proxy(data=data)
 
 
-@app.route(prefix + '/', methods=["DELETE"])
+@app.route(prefix + '/edit_group/', methods=["PUT"])
+@requires_auth
+def edit_group(**kwargs):
+    user_key_dict = {"user_key": kwargs.get("user_key")}
+    data = {**request.json, **user_key_dict}
+    return proxy(data=data)
+
+
+@app.route(prefix + '/delete_group/', methods=["DELETE"])
 @requires_auth
 def delete_group(**kwargs):
-    # user_key_dict = {"user_key": kwargs.get("user_key")}
-    user_key_dict = {"user_key": "UK4BSGB9"}
+    user_key_dict = {"user_key": kwargs.get("user_key")}
     data = {**request.json, **user_key_dict}
     return proxy(data=data)
 
@@ -45,8 +50,7 @@ def get_group(id, **kwargs):
 @app.route(prefix + '/add_professionals/', methods=["POST"])
 @requires_auth
 def add_professionals(**kwargs):
-    # user_key_dict = {"user_key": kwargs.get("user_key")}
-    user_key_dict = {"user_key": "UK4BSGB9"}
+    user_key_dict = {"user_key": kwargs.get("user_key")}
     data = {**request.json, **user_key_dict}
     return proxy(data=data)
 
@@ -54,7 +58,6 @@ def add_professionals(**kwargs):
 @app.route(prefix + '/remove_professionals/', methods=["DELETE"])
 @requires_auth
 def remove_professionals(**kwargs):
-    # user_key_dict = {"user_key": kwargs.get("user_key")}
-    user_key_dict = {"user_key": "UK4BSGB9"}
+    user_key_dict = {"user_key": kwargs.get("user_key")}
     data = {**request.json, **user_key_dict}
     return proxy(data=data)
