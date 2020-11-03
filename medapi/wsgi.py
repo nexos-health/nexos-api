104c4e72c1557c0b5d6c7c7d345f471876c87661
Elliott Lovell <elliott@repositpower.com>
2020-11-03 12:56:35 +1100

    Changed filename in logging handler as it seemed to be creating an error on the server

--- medapi/wsgi.py
+++ medapi/wsgi.py
@@ -5,7 +5,9 @@ import logging
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}}, expose_headers="Content-Type")
 
-logging_handler = logging.FileHandler(filename="/var/log/medplatform-api.log", encoding='utf-8', mode='a+')
+logging_handler = logging.FileHandler(
+    filename="./medplatform-api.log", encoding='utf-8', mode='a+'
+)
 logging.basicConfig(
     handlers=[logging_handler],
     format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
